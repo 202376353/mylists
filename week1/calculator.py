52a8a754e3f33371f862f45a10efac74bbba816f
202376353 <sinothandojohnson3@gmail.com>
2025-03-18 11:08:25 +0200

    Add calculator.py

---
+++ week1/calculator.py
@@ -0,0 +1,25 @@
+def calculator():
+    
+    num1 = float(input("Enter the first number: "))
+    operation = input("Enter the operation (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+
+   
+    if operation == '+':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+    elif operation == '-':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+    elif operation == '*':
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid operation. Please enter one of +, -, *, or /.")
+calculator()
